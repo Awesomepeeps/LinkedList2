3e8fe0117127d7621498213f0611ba2b11c7a63f
RiseSky <67874744+Awesomepeeps@users.noreply.github.com>
2024-01-07 23:34:35 -0800

    Finished edits

--- List.cpp
+++ List.cpp
@@ -7,21 +7,20 @@ using namespace std;
 void deleteNode(node* &head, node* current, node* previous, float studentID);
 void addNode(node* &head, node* current, node* newNode, node* previous);
 void searchNode(node* current);
-void deleteNodes(node* &head, node* current, node* previous);
+void deleteNodes(node* &head, node* current);
 void average(node* current, float total, float count);
+void searchSpecif(node* head, float ID);
 
 main (void) {
     node* head = NULL;
     char* input = new char(15);
 
     while (true) {
-        node* current = head;
-        node* previous = NULL;
-        cout << "What do you want to do (add, search, delete, average, quit)?" << endl;
+        cout << "What do you want to do (add, search, print, delete, average, quit)?" << endl;
         cin >> input;
 
         if (strcmp(input, "quit") == 0) {
-            deleteNodes(head, current, NULL);
+            deleteNodes(head, head);
             break;
         }
         else if (strcmp(input, "add") == 0) {
@@ -29,7 +28,7 @@ main (void) {
             char* lastName = new char(20);
             float gpa;
             int studentId;
-            cout << "Please enter first name of the student: " << endl;
+            cout << "\nPlease enter first name of the student: " << endl;
             cin >> firstName;
             cout << "Please enter last name of the student: " << endl;
             cin >> lastName;
@@ -37,108 +36,149 @@ main (void) {
             cin >> studentId;
             cout << "Please enter the GPA of the student: " << endl;
             cin >> gpa;
+            cout << endl;
             node* newNode = new node(new student(firstName, lastName, gpa, studentId));
-            addNode(head, current, newNode, previous);
+            addNode(head, head, newNode, head);
+        }
+        else if (strcmp(input, "print") == 0) {
+            if (head != NULL) {
+                cout << endl;
+                searchNode(head);
+            }
+            else {
+                cout << "\nThere are no students in the list\n" << endl;
+            }
         }
         else if (strcmp(input, "search") == 0) {
-            searchNode(head);
+            if (head != NULL) {
+                cout << "\nWhat is the ID of the student you want to search for?" << endl;
+                float ID = 0;
+                cin >> ID;
+                cout << endl;
+                searchSpecif(head, ID);
+                cout << endl;
+            }
+            else {
+                cout << "\nThere are no students in the list\n" << endl;
+            }
         }
         else if (strcmp(input, "delete") == 0) {
-            float studentID = 0;
-            cout << "What is the student ID of the student you want to delete?" << endl;
-            cin >> studentID;
-            deleteNode(head, current, NULL, studentID);
+            if (head != NULL) {
+                float studentID = 0;
+                cout << "\nWhat is the student ID of the student you want to delete?" << endl;
+                cin >> studentID;
+                deleteNode(head, head, head, studentID);
+                cout << endl;
+            }
+            else {
+                cout << "\nThere are no students in the list\n" << endl;
+            }
         }
         else if (strcmp(input, "average") == 0) {
-            average(head, 0, 0);
+            if (head != NULL) {
+                cout << endl;
+                average(head, 0, 0);
+                cout << endl;
+            }
+            else {
+                cout << "\nThere are no students in the list\n" << endl;
+            }
         }
     }
+
+    cout << "List has been deleted" << endl;
+    searchNode(head);
 }
 
-void deleteNodes(node* &head, node* current, node* previous) {
-
+void deleteNodes(node* &head, node* current) {
+    if (head->getNext() != NULL) {
+        float ID = head->getStudent()->getStudentID();
+        deleteNode(head, head, head, ID);
+        deleteNodes(head, head);
+    }
+    else {
+        float ID = head->getStudent()->getStudentID();
+        deleteNode(head, head, head, ID);
+    }
 }
 
 void addNode(node* &head, node* current, node* newNode, node* previous) {
-    cout << "Here" << endl;
     if (head == NULL) {
         head = newNode;
-        cout << "Here1" << endl;
     }
-    else if (newNode->getStudent()->getGPA() < current->getStudent()->getGPA()) {
-        cout << "Here2" << endl;
-        node* tmp = current;
-        cout << "Tmp ID: " << tmp->getStudent()->getStudentID() << endl;
-        previous->setNext(newNode);
-        cout << "Previous next id: " << previous->getNext()->getStudent()->getStudentID() << endl;
+    else if (newNode->getStudent()->getGPA() < current->getStudent()->getGPA() && newNode->getStudent()->getGPA() > previous->getStudent()->getGPA()) {
         newNode->setNext(current);
-        cout << "Ned node next id: " << newNode->getNext()->getStudent()->getStudentID() << endl;
-        cout << "Current next id: " << current->getNext()->getStudent()->getStudentID() << endl;
+        previous->setNext(newNode);
     }
-    else if (current->getNext() == NULL && previous != NULL) {
-        cout << "Here NULL2" << endl;
-        previous->setNext(newNode);
+        else if (current->getStudent()->getGPA() > newNode->getStudent()->getGPA()) {
+        head = newNode;
         newNode->setNext(current);
+    }
+    else if (current->getNext() == NULL) {
+        current->setNext(newNode);
     }
     else {
-        cout << "Here3" << endl;
-        if (current == NULL) {
-            cout << "Here4" << endl;
-            addNode(head, current->getNext(), newNode, head);
-        }
-        else {
-            cout << "Here5" << endl;
-            addNode(head, current->getNext(), newNode, previous->getNext());
-        }
+        addNode(head, current->getNext(), newNode, current);
     }
 }
 
 void searchNode(node* current) {
-    if (current->getNext() != NULL) {
+    if (current->getNext() == NULL) {
         current->getStudent()->printAll();
-        searchNode(current->getNext());
     }
     else {
         current->getStudent()->printAll();
+        searchNode(current->getNext());
+    }
+}
+
+void searchSpecif(node* head, float ID) {
+    if (head->getStudent()->getStudentID() == ID) {
+        head->getStudent()->printAll();
+    }
+    else if (head->getNext() == NULL) {
+        cout << "Student is not in list" << endl;
+    }
+    else {
+        searchSpecif(head->getNext(), ID);
     }
 }
 
 void deleteNode(node* &head, node* current, node* previous, float studentID) {
     if (head == NULL) {
-        return;
+        cout << "There are no students in the list" << endl;
     }
-    else if (studentID == current->getStudent()->getGPA()) {
-        node* tmp = current;
-        //cout << tmp->getStudent()->getStudentID() << endl;
-        previous->setNext(tmp->getNext());
-        delete tmp;
-        //cout << previous->getNext()->getStudent()->getStudentID() << endl;
-        //newNode->setNext(tmp);
-        //cout << newNode->getNext()->getStudent()->getStudentID() << endl;
-        //cout << "Here2" << endl;
-        //cout << current->getNext()->getStudent()->getStudentID() << endl;
+    else if (head->getStudent()->getStudentID() == studentID) {
+        node* tmp = head->getNext();
+        delete head;
+        head = tmp;
+    }
+    else if (current->getStudent()->getStudentID() == studentID && previous->getNext()->getStudent()->getStudentID() == studentID) {
+        previous->setNext(current->getNext());
+        delete current;
+        current = previous->getNext();
+    }
+    else if (current->getStudent()->getStudentID() == studentID && current->getNext() == NULL) {
+        delete current;
+        previous->setNext(NULL);
     }
     else {
-        cout << "Here3" << endl;
-        if (current == NULL) {
-            deleteNode(head, current->getNext(), previous, studentID);
-            cout << "Here4" << endl;
-        }
-        else {
-            deleteNode(head, current->getNext(), previous->getNext(), studentID);
-            cout << "Here5" << endl;
-        }
+        deleteNode(head, current->getNext(), current, studentID);
     }
 }
 
 void average(node* current, float total, float count) {
-    while (current->getNext() != NULL) {
-        //cout << "Total bfr: " << total << endl;
+
+    if (current->getNext() == NULL) {
         total = total + current->getStudent()->getGPA();
-        //cout << "Total aft: " << total << endl;
-        //cout << "GPA std: " << current->getStudent()->getGPA() << endl;
         count++;
-        //cout << "Count is: " << count << "\nAverage is : " << total/count << endl;
+        cout << "The total grade is: " << total << endl;
+        cout << "The count grade is: " << count << endl;
+        cout << "The average grade is: " << total/count << endl;
+    }
+    else {
+        total = total + current->getStudent()->getGPA();
+        count++;
         average(current->getNext(), total, count);
     }
 }
